6dc3e871fe44283173b17ecd20337bed07fa9338
FPtje <fpeijnenburg@gmail.com>
2013-08-21 13:01:14 +0200

    Extra F4 menu tab example

---
+++ lua/darkrp_modules/extraf4tab/cl_fancytab.lua
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------
+F4 menu tab modification module.
+---------------------------------------------------------------------------*/
+
+if true then return end -- REMOVE THIS LINE TO ENABLE THIS MODULE
+
+local url = "http://wiki.darkrp.com/index.php/Main_Page"
+local tabName = "MOTD"
+
+
+local function createF4MenuTab()
+	// DarkRP.switchTabOrder(2, 3) -- Remove the "//" in this line if you want to move the third tab to the left of the second tab!
+	// DarkRP.removeF4MenuTab("Ammo") -- Remove the "//" in this line if you want to remove the Ammo tab!
+
+	local webPage = vgui.Create("F1HTML")
+	webPage:OpenURL(url)
+	DarkRP.addF4MenuTab(tabName, webPage)
+end
+hook.Add("F4MenuTabs", "MyCustomF4MenuTab", createF4MenuTab)
---
+++ lua/darkrp_modules/extraf4tab/readme.txt
@@ -0,0 +1,9 @@
+/*---------------------------------------------------------------------------
+/*---------------------------------------------------------------------------
+F4 Mod example
+---------------------------------------------------------------------------
+---------------------------------------------------------------------------*/
+
+This is an example module that adds an extra tab and shows some more F4 menu things you can do
+
+This is to make F4 editing easier for you. It also shows that it can be done without having to modify the existing F4 menu.
