28967f5ba25e4b39dd0aee2d62f79ddc342cde49
FPtje <fpeijnenburg@gmail.com>
2013-08-24 19:02:38 +0200

    Fixed #3

--- lua/darkrp_customthings/groupchats.lua
+++ lua/darkrp_customthings/groupchats.lua
@@ -8,12 +8,12 @@ talk to one another through /g or team chat.
 
 HOW TO MAKE A GROUP CHAT:
 Simple method:
-GM:AddGroupChat(List of team variables separated by comma)
+GAMEMODE:AddGroupChat(List of team variables separated by comma)
 
 Advanced method:
-GM:AddGroupChat(a function with ply as argument that returns whether a random player is in one chat group)
+GAMEMODE:AddGroupChat(a function with ply as argument that returns whether a random player is in one chat group)
 This is for people who know how to script Lua.
 
 ---------------------------------------------------------------------------*/
--- Example: GM:AddGroupChat(TEAM_MOB, TEAM_GANG)
--- Example: GM:AddGroupChat(function(ply) return ply:isCP() end)
+-- Example: GAMEMODE:AddGroupChat(TEAM_MOB, TEAM_GANG)
+-- Example: GAMEMODE:AddGroupChat(function(ply) return ply:isCP() end)
