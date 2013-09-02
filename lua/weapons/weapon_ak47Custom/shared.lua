8430260498d49cfcf295593e4b0c8e13ef0c6eec
FPtje <fpeijnenburg@gmail.com>
2013-09-02 19:43:32 +0200

    Better fix for #8

--- lua/weapons/weapon_ak47Custom/shared.lua
+++ lua/weapons/weapon_ak47Custom/shared.lua
@@ -20,6 +20,7 @@ SWEP.Base = "weapon_cs_base2"
 SWEP.Spawnable = true
 SWEP.AdminSpawnable = true
 SWEP.Category = "DarkRP (Weapon)"
+SWEP.SpawnMenuIcon = "vgui/entities/weapon_ak472"
 
 SWEP.UseHands = true
 SWEP.ViewModel = "models/weapons/cstrike/c_rif_ak47.mdl"
