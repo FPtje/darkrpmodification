c43e177f706c528593b1b93a8df4976094c87bde
Robert Van Loon <lithinzore09@gmail.com>
2014-10-02 23:07:05 -0500

    Update jobs.lua

--- lua/darkrp_customthings/jobs.lua
+++ lua/darkrp_customthings/jobs.lua
@@ -17,7 +17,49 @@ http://wiki.darkrp.com/index.php/DarkRP:CustomJobFields
 
 Add jobs under the following line:
 ---------------------------------------------------------------------------*/
+Peasant = DarkRP.createJob("Peasant", {
+        color = Color(255, 255, 255, 255),
+        model = {
+                "models/player/Group03/Female_01.mdl",
+                "models/player/Group03/Female_02.mdl"
+                },
+        description = "You are common peasant, the very backbone of the economy. Now get to work, peon!",
+        weapons = {""},
+        command = "Peasant",
+        max = 15,
+        salary = 45,
+        admin = 0,
+        vote = false,
+        hasLicense = false,
+        modelScale = 1.2,
+        maxpocket = 5,
+        maps = {"rp_medievalrp_v2"},
+        candemote = false,
+        mayor = false,
+        chief = false,
+        medic = false,
+        cook = false,
+        hobo = false,
 
+        CanPlayerSuicide = function(ply) return false end,
+        PlayerCanPickupWeapon = function(ply, weapon) return true end,
+        PlayerDeath = function(ply, weapon, killer) end,
+        PlayerLoadout = function(ply) return true end,
+        PlayerSelectSpawn = function(ply, spawn) end,
+        PlayerSetModel = function(ply) return "models/player/Group03/Female_02.mdl" end,
+        PlayerSpawn = function(ply) end,
+        PlayerSpawnProp = function(ply, model) end,
+        RequiresVote = function(ply, job) for k,v in pairs(player.GetAll()) do if IsValid(v) and v:IsAdmin() then return false end end return true end, -- People need to make a vote when there is no admin around
+        ShowSpare1 = function(ply) end,
+        ShowSpare2 = function(ply) end,
+        canStartVote = function(ply) return ply:Distance(SomeNPC) < 200 end,
+        canStartVoteReason = "Must be close to some NPC",
+        buttonColor = Color(255, 255, 255, 255), -- The color of the button in the F4 menu
+        label = "Super job", -- Optional: the text on the button in the F4 menu
+        ammo = {
+                ["pistol"] = 60,
+        },
+})
 
 
 
