b58ac15473243d566025500f6da00da6fe0efe5a
FPtje <fpeijnenburg@gmail.com>
2015-01-28 21:11:29 +0100

    Fixed voterestrictions. #54

--- lua/darkrp_modules/voterestrictions/sv_init.lua
+++ lua/darkrp_modules/voterestrictions/sv_init.lua
@@ -8,20 +8,25 @@ Vote regulations
 Assumes the jobs are unedited in jobs.lua, add them in the tables below to add your own jobs
 ---------------------------------------------------------------------------*/
 
-if true then return end -- REMOVE THIS LINE TO ENABLE THE MODULE
+--if true then return end -- REMOVE THIS LINE TO ENABLE THE MODULE
+local GovernmentJobs, GangsterJobs
 
-local GovernmentJobs = {
-	[TEAM_MAYOR] = true,
-	[TEAM_POLICE] = true,
-	[TEAM_CHIEF] = true
-}
+local function config()
+	GovernmentJobs = {
+		[TEAM_MAYOR] = true,
+		[TEAM_POLICE] = true,
+		[TEAM_CHIEF] = true
+	}
 
-local GangsterJobs = {
-	[TEAM_GANG] = true,
-	[TEAM_MOB] = true
-}
+	GangsterJobs = {
+		[TEAM_GANG] = true,
+		[TEAM_MOB] = true
+	}
+end
 
 local function decide(ply, target)
+	if not GovernmentJobs then config() end
+
 	if ply:Team() == TEAM_HOBO then return false, DarkRP.getPhrase("hobos_no_rights") end
 
 	if GangsterJobs[ply:Team()] and GovernmentJobs[target:Team()] then
@@ -32,7 +37,7 @@ local function decide(ply, target)
 		return false, DarkRP.getPhrase("government_cant_vote_for_gangsters")
 	end
 end
-hook.Add("CanDemote", "VoteRegulations", decide)
+hook.Add("canDemote", "VoteRegulations", decide)
 
 local function canVote(ply, vote)
 	if vote.votetype ~= "job" and vote.votetype ~= "demote" then return end -- only apply to promotions and demotions
