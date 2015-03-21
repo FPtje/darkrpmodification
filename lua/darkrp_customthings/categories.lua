d01fb45cff267d4a75e44c01b0edc92705f50ee7
FPtje <fpeijnenburg@gmail.com>
2015-03-21 20:02:36 +0100

    Explanation in categories.lua

--- lua/darkrp_customthings/categories.lua
+++ lua/darkrp_customthings/categories.lua
@@ -6,6 +6,18 @@ The categories of the default F4 menu.
 Please read this page for more information:
 http://wiki.darkrp.com/index.php/DarkRP:Categories
 
+In case that page can't be reached, here's an example with explanation:
+
+DarkRP.createCategory{
+    name = "Citizens", -- The name of the category.
+    categorises = "jobs", -- What it categorises. MUST be one of "jobs", "entities", "shipments", "weapons", "vehicles", "ammo".
+    startExpanded = true, -- Whether the category is expanded when you open the F4 menu.
+    color = Color(0, 107, 0, 255), -- The color of the category header.
+    canSee = function(ply) return true end, -- OPTIONAL: whether the player can see this category AND EVERYTHING IN IT.
+    sortOrder = 100, -- OPTIONAL: With this you can decide where your category is. Low numbers to put it on top, high numbers to put it on the bottom. It's 100 by default.
+}
+
+
 Add new categories under the next line!
 ---------------------------------------------------------------------------]]
 
