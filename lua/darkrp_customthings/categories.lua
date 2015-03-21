e4501336b73c2ac9e8515409c20ab38e39a54f17
FPtje <fpeijnenburg@gmail.com>
2015-03-21 19:21:54 +0100

    Categories

---
+++ lua/darkrp_customthings/categories.lua
@@ -0,0 +1,11 @@
+--[[-----------------------------------------------------------------------
+Categories
+---------------------------------------------------------------------------
+The categories of the default F4 menu.
+
+Please read this page for more information:
+http://wiki.darkrp.com/index.php/DarkRP:Categories
+
+Add new categories under the next line!
+---------------------------------------------------------------------------]]
+
