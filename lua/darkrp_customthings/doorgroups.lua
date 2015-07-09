d49f417843ae91a9e3c754a67732f7f78d7ccb6c
Soviet-Ivan <ivanguydukov31@gmail.com>
2015-07-08 22:27:50 -0400

    Update doorgroups.lua

--- lua/darkrp_customthings/doorgroups.lua
+++ lua/darkrp_customthings/doorgroups.lua
@@ -7,7 +7,7 @@ The server owner can set certain doors as owned by a group of people, identified
 HOW TO MAKE A DOOR GROUP:
 AddDoorGroup("NAME OF THE GROUP HERE, you will see this when looking at a door", Team1, Team2, team3, team4, etc.)
 ---------------------------------------------------------------------------]]
-
+AddDoorGroup("Government Officials", TEAM_CHIEF, TEAM_POLICE, TEAM_MAYOR)
 
 -- Example: AddDoorGroup("Cops and Mayor only", TEAM_CHIEF, TEAM_POLICE, TEAM_MAYOR)
 -- Example: AddDoorGroup("Gundealer only", TEAM_GUN)
