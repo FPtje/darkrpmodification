0010d4ad1a5406bdf48faae867ad490bf6f7cb72
Servius <nosamrj@gmail.com>
2017-03-01 04:24:51 -0800

    sh_weaponsettings.lua documentation expanded; Custom Ak47 now a better example weapon with comments explaing functions.

--- lua/darkrp_modules/weaponsettings/sh_weaponsettings.lua
+++ lua/darkrp_modules/weaponsettings/sh_weaponsettings.lua
@@ -10,14 +10,20 @@ This module allows you to change the settings of weapons. Here's how it works.
 Per weapon and setting a line is added in this format:
 weapon "WEAPON CLASS NAME HERE".SETTING_HERE = VALUE_HERE
 
+
+
 IMPORTANT:
     - Note the quotation marks around the weapon class name!
     - Note the '.' between the class name and the setting!
     - Casing MATTERS. PrintName IS NOT THE SAME AS printname!
     - CHANGING THE SETTINGS IS NOT RECODING THE WEAPON. PLEASE LEAVE THE AUTHORS FIELD INTACT. THANK YOU.
     - Technically you can set any field of the weapon. Advanced users can even override e.g. PrimaryAttack with this.
+	- This system is not limited to weapons that come with the DarkRP gamemode. 
 
 Examples:
+
+-- This essentially works by replacing "SWEP" at the start of each setting like "SWEP.Primary.Ammo = "pistol""
+with your customization in this file being "weapon_deagle2".Primary.Ammo = "ar2"
 
 -- Set the printname of the stunstick to "bash baton". Note: the quotation marks are important!
 weapon "stunstick".PrintName = "Bash baton"
@@ -31,11 +37,14 @@ weapon "weapon_p2282".Primary.Damage = 20
 
 LIST OF DEFAULT DARKRP WEAPONS:
 The class names of the default DarkRP weapons are listed on this page:
-https://github.com/FPtje/DarkRP/tree/master/entities/weapons
+https://github.com/FPtje/DarkRP/tree/master/entities/weapons 
 
-Note that it's the folder names that matter here, e.g. keys, med_kit, door_ram, weapon_m42 etc.
+You can find extra documentation for weapon functions here on this page: 
+https://github.com/FPtje/darkrpmodification/blob/master/lua/weapons/weapon_ak47custom/shared.lua
 
-SOME SETTINGS THAT CAN BE CHANGED USING THIS METHOD:
+Note that it's the folder names that matter here(the folders that contain the shared.lua for the weapons), e.g. keys, med_kit, door_ram, weapon_m42 etc.
+
+SOME EXAMPLE SETTINGS THAT CAN BE CHANGED USING THIS METHOD:
     - PrintName
     - Instructions
     - Contact
@@ -50,21 +59,6 @@ SOME SETTINGS THAT CAN BE CHANGED USING THIS METHOD:
     - DefaultClip
 
 This list is not complete.
-A more complete list can be found at the following links(Note, not every variable is universal. Make sure to check documentation for the weapon you are modifiying!): 
-
-TFA Base:
-https://bitbucket.org/TheForgottenArchitect/tfa-base/src/463adebf0d55941361f7580d9eade151b658b7e1/lua/weapons/tfa_base_template/shared.lua?at=master&fileviewer=file-view-default
-
-M9K Base: 
-http://murderthon9000.com/bobs-base/
-
-CW 2.0: 
-https://docs.google.com/document/d/1FC6Lm7ml6eI-ocwSk5GXq5Sv0hZyrfQK9bMkChHhNNk/edit
-
-V92 SWEP Base: 
-http://steamcommunity.com/groups/CultOfV92/discussions/0/360671727314185116/
-
-Waiting on response from FA:S 2.0 Devs. 
 
 -- ADD WEAPON SETTINGS BELOW THIS LINE
 ]] -----------------------------------------
