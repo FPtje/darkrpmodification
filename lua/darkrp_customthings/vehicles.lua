17495cdb9374e23063170949bbcd2c103252892e
Florian Dubois <floriandubois115@gmail.com>
2020-07-25 20:00:42 +0200

    Update DarkRP wiki link

--- lua/darkrp_customthings/vehicles.lua
+++ lua/darkrp_customthings/vehicles.lua
@@ -6,7 +6,7 @@ This file contains your custom vehicles.
 This file should also contain vehicles from DarkRP that you edited.
 
 For examples and explanation please visit this wiki page:
-http://wiki.darkrp.com/index.php/DarkRP:Vehicles
+https://darkrp.miraheze.org/wiki/DarkRP:Vehicles
 
 If you want to keep the vehicle code and not comment it out you can add a vehicle to the "DarkRP.disabledDefaults["vehicles"]" section
 in the disabled_defaults.lua in the "darkrp_config" folder
