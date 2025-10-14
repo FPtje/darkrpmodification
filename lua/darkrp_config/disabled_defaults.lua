52b6ccfd80005401546e60947a6b4d9488edefab
marselmarkovic4-source <marselmarkovic4@gmail.com>
2025-10-14 22:34:13 +0400

    Update disabled_defaults.lua

--- lua/darkrp_config/disabled_defaults.lua
+++ lua/darkrp_config/disabled_defaults.lua
@@ -23,11 +23,11 @@ In some cases some external addons may overwrite one of the modules below and wi
 In these few cases it will be pretty obvious when you expect something to happen and nothing does.
 ---------------------------------------------------------------------------]]
 DarkRP.disabledDefaults["modules"] = {
-    ["afk"]              = true,
+    ["afk"]              = ,3
     ["chatsounds"]       = false,
     ["events"]           = false,
     ["fpp"]              = false,
-    ["f1menu"]           = false,
+    ["f1menu"]           = 5,
     ["f4menu"]           = false,
     ["hitmenu"]          = false,
     ["hud"]              = false,
